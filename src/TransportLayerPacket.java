790bfca5e890a17f44e5210d280b19fb28691365
Jackson Blair <jackson.blair.2019@uni.strath.ac.uk>
2021-10-20 17:06:11 +0100

    Made additional methods, fields and constructors. Currently unsure on status.

--- src/TransportLayerPacket.java
+++ src/TransportLayerPacket.java
@@ -4,13 +4,33 @@ public class TransportLayerPacket {
     // You may need extra fields
     private int seqnum;
     private int acknum;
+    private int sourcePort;
+    private int destinationPort;
 
     byte[] data;
 
     // You may need extra methods
 
+    public TransportLayerPacket(int length) {
+        seqnum = 0;
+        acknum = 0;
+        sourcePort = 0;
+        destinationPort = 0;
+        data = new byte[length];
+    }
+
     public TransportLayerPacket(TransportLayerPacket pkt) {
-        // complete this method
+            if (pkt == null) {
+                seqnum = 0;
+                acknum = 0;
+                data = new byte[32];
+            } else {
+                this.seqnum = pkt.getSeqnum();
+                this.acknum = pkt.getAcknum();
+                this.sourcePort = pkt.getSourcePort();
+                this.destinationPort = pkt.getDestinationPort();
+                this.data = pkt.getData();
+            }
     }
 
     public void makePkt(int seqNum, byte[] data){
@@ -27,8 +47,36 @@ public class TransportLayerPacket {
         this.acknum = acknum;
     }
 
+    public void setSourcePort(int source) {
+        this.sourcePort = source;
+    }
+
+    public void setDestinationPort(int destination) {
+        this.destinationPort = destination;
+    }
+
+    public int getSeqnum() {
+        return this.seqnum;
+    }
+
+    public int getAcknum() {
+        return this.acknum;
+    }
+
+    public int getSourcePort() {
+        return this.sourcePort;
+    }
+
+    public int getDestinationPort() {
+        return this.destinationPort;
+    }
+
     public byte[] getData() {
         return data;
+    }
+
+    public int getByteLength() {
+        return data.length;
     }
 
 }
