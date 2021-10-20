8e797e4f6835d99b26ae5fc30c64683979a9172e
Jackson Blair <jackson.blair.2019@uni.strath.ac.uk>
2021-10-21 00:13:49 +0100

    Added Additional fields and methods, including those around header and tail of packet

--- src/TransportLayerPacket.java
+++ src/TransportLayerPacket.java
@@ -6,8 +6,12 @@ public class TransportLayerPacket {
     private int acknum;
     private int sourcePort;
     private int destinationPort;
+    private int sourceIP;
+    private int destinationIP;
 
     byte[] data;
+    int headerLength;
+    int tailLength;
 
     // You may need extra methods
 
@@ -15,18 +19,32 @@ public class TransportLayerPacket {
         seqnum = 0;
         acknum = 0;
         sourcePort = 0;
+        sourceIP = 0;
         destinationPort = 0;
+        destinationIP = 0;
+        //Values used as placeholders
         data = new byte[length];
+        headerLength = 30;
+        tailLength = 30;
     }
 
     public TransportLayerPacket(TransportLayerPacket pkt) {
             if (pkt == null) {
                 seqnum = 0;
                 acknum = 0;
+                sourcePort = 0;
+                sourceIP = 0;
+                destinationPort = 0;
+                destinationIP = 0;
+                //Values used as placeholders
                 data = new byte[32];
+                headerLength = 30;
+                tailLength = 30;
             } else {
                 this.seqnum = pkt.getSeqnum();
                 this.acknum = pkt.getAcknum();
+                this.sourceIP = pkt.getSourceIP();
+                this.destinationIP = pkt.getDestinationIP();
                 this.sourcePort = pkt.getSourcePort();
                 this.destinationPort = pkt.getDestinationPort();
                 this.data = pkt.getData();
@@ -55,12 +73,28 @@ public class TransportLayerPacket {
         this.destinationPort = destination;
     }
 
+    public void setSourceIP(int source) {
+        this.sourceIP = source;
+    }
+
+    public void setDestinationIP(int destination) {
+        this.destinationIP = destination;
+    }
+
     public int getSeqnum() {
         return this.seqnum;
     }
 
     public int getAcknum() {
         return this.acknum;
+    }
+
+    public int getSourceIP() {
+        return this.sourceIP;
+    }
+
+    public int getDestinationIP() {
+        return this.destinationIP;
     }
 
     public int getSourcePort() {
@@ -77,6 +111,30 @@ public class TransportLayerPacket {
 
     public int getByteLength() {
         return data.length;
+    }
+
+    public int getHeaderLength() {
+        return this.headerLength;
+    }
+
+    public int getTailLength() {
+        return this.tailLength;
+    }
+
+    public byte[] getHeader() {
+        byte[] result = new byte[this.headerLength];
+        for (int i = 0; i < this.headerLength; i++) {
+            result[i] = data[i];
+        }
+        return result;
+    }
+
+    public byte[] getTail() {
+        byte[] result = new byte[this.tailLength];
+        for (int i = 0; i < this.tailLength; i++) {
+            result[i] = data[data.length - (i + 1)];
+        }
+        return result;
     }
 
 }
