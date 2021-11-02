1136c685487755144997440eef5bae0495353221
Nuoxu <liantinghan@gmail.com>
2021-11-02 16:01:58 +0000

    the problem of incorrect checksum still to be fixed, but I have added commentary to help others to understand my code.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        NetworkSimulator sim = new NetworkSimulator(4, 0.0, 0.0, 10.0, false, 4);
+
+        // TODO: Ask how does loss and corruption probability works.
+        //  when test for packet loss scenarios even with lossProb being set to 1.0 it still goes to an infinite loop of resend and then loss
+        NetworkSimulator sim = new NetworkSimulator(2, 0.0, 50.0, 10.0, false, 4);
 
 
         // TODO: Set the sender   (sim.setSender)
