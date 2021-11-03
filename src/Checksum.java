87103b52f3e39131692f97b491818763109d9fc2
Nuoxu <liantinghan@gmail.com>
2021-11-03 17:44:44 +0000

    S&W stage is complete.

--- src/Checksum.java
+++ src/Checksum.java
@@ -2,97 +2,94 @@ public class Checksum {
 
     byte[] dataBytes;
 
+    /*
+     * get data into Checksum
+     */
     public Checksum(byte[] data) {
         this.dataBytes = data;
     }
 
+    /*
+     *
+     */
     public String createCheckSum() {
+        String answer = "0000000000000000";
 
-        String answer = "00000000";
+        for (byte dataByte : this.dataBytes) {
+            //convert each byte into binary
+            String one = String.format("%16s", Integer.toBinaryString(dataByte & 255)).replace(' ', '0');
 
-        for(int i = 0; i<dataBytes.length; i++){
+            //add two byte at a time
+            answer = this.bitAddition(one, answer);
 
-            String one = String.format("%8s", Integer.toBinaryString(dataBytes[i] & 0xFF)).replace(' ', '0');
-            answer = bitAddition(one,answer);
         }
 
-        return onesComplement(answer);
+        //having ana answer
+        answer = this.onesComplement(answer);
+
+        return answer;
     }
 
-
-    public String bitAddition(String first, String second){
-
+    public String bitAddition(String first, String second) {
         int c = 0;
-
         String answer = "";
 
-        for(int i = first.length()-1; i>=0; i--) {
-
+        for(int i = first.length() - 1; i >= 0; --i) {
             int a = Integer.parseInt(Character.toString(first.charAt(i)));
             int b = Integer.parseInt(Character.toString(second.charAt(i)));
-
             int sum = a + b;
-            //Adds the Carry
-            if (c !=0) {
-                sum = sum + c;
+            if (c != 0) {
+                sum += c;
             }
 
-            //Adds the string
             if (sum == 2) {
                 c = 1;
-                answer =  "0" + answer ;
+                answer = "0" + answer;
             } else if (sum == 3) {
                 c = 1;
-                answer =  "1" + answer;
-            } else if(sum == 0) {
+                answer = "1" + answer;
+            } else if (sum == 0) {
                 c = 0;
-                answer =  "0" + answer;
-            }
-            else if(sum== 1){
+                answer = "0" + answer;
+            } else if (sum == 1) {
                 c = 0;
                 answer = "1" + answer;
             }
-
-
         }
 
-        String cString = String.format("%8s", Integer.toBinaryString(c & 0xFF)).replace(' ', '0');
+        String cString = String.format("%16s", Integer.toBinaryString(c & 255)).replace(' ', '0');
+        if (cString.equals("0000000000000000")) {
 
-        if(cString.equals("00000000")){
             return answer;
-        }
-        else {
-            return bitAddition(answer,cString);
+        } else {
+
+            return this.bitAddition(answer, cString);
         }
     }
 
-
     public String createTotal() {
+        String answer = "0000000000000000";
 
-        String answer = "00000000";
+        for (byte dataByte : this.dataBytes) {
+            String one = String.format("%16s", Integer.toBinaryString(dataByte & 255)).replace(' ', '0');
+            answer = this.bitAddition(one, answer);
 
-        for(int i = 0; i<dataBytes.length; i++){
-            String one = String.format("%8s", Integer.toBinaryString(dataBytes[i] & 0xFF)).replace(' ', '0');
-            answer = bitAddition(one,answer);
         }
 
         return answer;
     }
 
-    public String onesComplement(String change){
-
+    public String onesComplement(String change) {
         char[] binaryArray = change.toCharArray();
 
-        for(int i = 0; i<binaryArray.length; i++){
-
-            if(binaryArray[i] == '1'){
-
+        for(int i = 0; i < binaryArray.length; ++i) {
+            if (binaryArray[i] == '1') {
                 binaryArray[i] = '0';
-            }
-            else{
+            } else {
                 binaryArray[i] = '1';
             }
         }
+
         StringBuilder answer = new StringBuilder();
         answer.append(binaryArray);
         return answer.toString();
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        NetworkSimulator sim = new NetworkSimulator(1, 0.0, 1.0, 10.0, false, 4);
+        NetworkSimulator sim = new NetworkSimulator(2, 0.3, 0.25, 10.0, false, 4);
 
 
         // TODO: Set the sender   (sim.setSender)
